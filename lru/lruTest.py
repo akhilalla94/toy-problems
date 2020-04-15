dad80a6e423eab8cdf069954eb634a8e45345ea5
akhilalla94 <akhilalla1994@msitprogram.net>
2020-04-15 15:37:37 +0530

    Added assert get-cache method

--- lru/lruTest.py
+++ lru/lruTest.py
@@ -9,13 +9,18 @@ class lruTest:
     def testcases(self):
         a = lru(3)
         a.put("google")
+        # print(a.get_cache())
+        assert a.get_cache() == ['google-www.google.com'],"testcase A failed"
         assert a.get("google") == "www.google.com", "testcase 1 failed"
         print("testcase 1 passed")
         a.put("facebook")
+        # print(a.get_cache())
+        assert a.get_cache() == ['facebook-www.facebook.com','google-www.google.com'],"testcase B failed"
         assert a.get("facebook") == "www.facebook.com", "testcase 2 failed"
         print("testcase 2 passed")
         a.put("gmail")
         a.put("youtube")
+        assert a.get_cache() == ['youtube-www.youtube.com','gmail-www.gmail.com','facebook-www.facebook.com'],"testcase C failed"
         assert a.get("google") == None, "testcase 3 failed"
         print("testcase 3 passed")
         print("All Testcases passed!!")
