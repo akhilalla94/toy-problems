6a15b5889e065429f73506b89cf078916a10a096
akhilalla94 <akhilalla1994@msitprogram.net>
2020-04-28 22:45:49 +0530

    Done

---
+++ bean.js
@@ -0,0 +1,21 @@
+function countBs(s) {
+    var count = 0; 
+    for (var i = 0; i < s.length; i += 1) {
+      if (s.charAt(i) === "B")
+        count += 1;
+    }
+    return count;
+  }
+  console.log(countBs("ABBBCCCBBBA"));
+
+  
+  function countChar(s, c) {
+    var count = 0; 
+    for (var i = 0; i < s.length; i += 1) {
+      if (s.charAt(i) === c)
+        count += 1;
+    }
+    return count;
+  }
+  console.log(countChar("ALLAAKHIL", "L"));
+  
